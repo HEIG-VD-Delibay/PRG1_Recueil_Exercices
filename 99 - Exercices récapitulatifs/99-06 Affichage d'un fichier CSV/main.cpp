b0e871b2170e89596b6534e7ef9c5f8bf2ab0ba5
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2025-08-05 00:40:02 +0200

    Fix '99-06 Affichage d'un fichier CSV' with .csv extension and prefix in output to avoid trimming

--- 99 - Exercices récapitulatifs/99-06 Affichage d'un fichier CSV/main.cpp
+++ 99 - Exercices récapitulatifs/99-06 Affichage d'un fichier CSV/main.cpp
@@ -4,8 +4,9 @@
 using namespace std;
 
 int main() {
+    printf("Content of data.csv\n");
     try {
-        display_csv("data.txt", ',', 10);
+        display_csv("data.csv", ',', 10);
     } catch (File_not_found &e) {
         cerr << "File not found \n";
         exit(-1);
